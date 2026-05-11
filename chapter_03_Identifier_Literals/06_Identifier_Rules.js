0ab138b754ce304931f961ba24c529c454f6033a
ASWATHYMK20 <aswathyachu200697@gmail.com>
2026-05-12 00:12:44 +0530

    Add JS basics and identifier rules examples

---
+++ chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,11 @@
+var $=10;
+var _a=12;
+var p=10;
+var ab123=34;
+
+var Name="Aswathy";
+var name="MK";
+  //var pramod dutta="hello";
+var pramod_dutta="hello";
+var pramod$dutta="hello";
+var pramodu1232="hello";
